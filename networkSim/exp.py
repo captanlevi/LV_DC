014ec9cbe658c632647281a9a300789936191ee7
rushi <rjlinkin50@gmail.com>
2026-03-06 11:14:18 +1100

    path changes to make it work for students

--- networkSim/exp.py
+++ networkSim/exp.py
@@ -11,8 +11,9 @@ import signal
 
 @dataclass
 class NetLabel:
-    timestamp : float
-    speed : int
+    timestamp: float
+    speed: int
+
 
 def run_(cmd):
     print(f"> {cmd}")
@@ -20,10 +21,7 @@ def run_(cmd):
 
 
 def run(args):
-    proc = subprocess.Popen(
-        args,
-        preexec_fn=os.setsid  # NEW process group
-    )
+    proc = subprocess.Popen(args, preexec_fn=os.setsid)  # NEW process group
     try:
         proc.wait()
     except KeyboardInterrupt:
@@ -40,6 +38,7 @@ def initialize_network() -> bool:
         print(e)
         return False
 
+
 def finalize_network() -> bool:
     try:
         run(["make", "disable"])
@@ -48,6 +47,7 @@ def finalize_network() -> bool:
         print(e)
         return False
 
+
 def initialize_pcap() -> bool:
     try:
         run(["make", "start-capture"])
@@ -55,7 +55,8 @@ def initialize_pcap() -> bool:
     except Exception as e:
         print(e)
         return False
-    
+
+
 def finalize_pcap() -> bool:
     try:
         run(["make", "stop-capture"])
@@ -66,11 +67,15 @@ def finalize_pcap() -> bool:
         return False
 
 
-def ossilate(stable_time_in_seconds : int, net_labels : list[NetLabel],rates = [5000,1000,500,200,100]):
+def ossilate(
+    stable_time_in_seconds: int,
+    net_labels: list[NetLabel],
+    rates=[5000, 1000, 500, 200, 100],
+):
 
     index = 0
     while True:
-        index = index%len(rates)
+        index = index % len(rates)
         rate = rates[index]
         run(["make", "slow", f"BANDWIDTH={rate}kbit"])
         ts = time.time()
@@ -79,27 +84,28 @@ def ossilate(stable_time_in_seconds : int, net_labels : list[NetLabel],rates = [
         index += 1
 
 
-def save_json(path : str, data : list[NetLabel]):
-    save_data  = [asdict(obj) for obj in data]
-    with open(path,"w") as f:
+def save_json(path: str, data: list[NetLabel]):
+    save_data = [asdict(obj) for obj in data]
+    with open(path, "w") as f:
         for item in save_data:
             json.dump(item, f)
             f.write("\n")
 
+
 if __name__ == "__main__":
 
-    is_capture : bool = True
+    is_capture: bool = True
 
     net_labels = []
     if initialize_network() == False:
         raise ValueError("Cannot initialize the network for collection")
-    
+
     # PCAP is always done after the network initialization
     if is_capture and initialize_pcap() == False:
         raise ValueError("Cannot initialize pcap capture")
 
     try:
-        ossilate(stable_time_in_seconds= 30, net_labels= net_labels)
+        ossilate(stable_time_in_seconds=30, net_labels=net_labels)
 
     finally:
         # Need to stop capture before tearing down the network
@@ -107,6 +113,5 @@ if __name__ == "__main__":
             raise ValueError("Cannot finalize pcap capture")
         if finalize_network() == False:
             raise ValueError("Could not finish removing the network setup")
-      
 
-        save_json(path= "net_labels.txt", data= net_labels)
+        save_json(path="../current_data/net_labels.txt", data=net_labels)
